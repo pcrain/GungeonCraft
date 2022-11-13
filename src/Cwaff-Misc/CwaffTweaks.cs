eaec2409035cf5014d0f3a1081d22d521d5849f4
Patrick Crain <patrickacrain@gmail.com>
2022-11-13 00:37:57 -0600

    can now add custom petting animations!

--- src/Cwaff-Misc/CwaffTweaks.cs
+++ src/Cwaff-Misc/CwaffTweaks.cs
@@ -10,87 +10,84 @@ using System.Reflection;  //debug
 
 namespace CwaffingTheGungy
 {
-    class CwaffTweaks
+    public static class CwaffTweaks
     {
+        private static DirectionalAnimation dogPettingAnimation;
+
         public static void Init()
         {
-            //Get base companion controller from doggo
+            GetDogPettingAnimation();
+
+            //Make Wolf pettable
+            ETGModConsole.Log("Making wolfyboi pettable");
+            Gungeon.Game.Items["wolf"].GetComponent<CompanionItem>().MakePettable();
+        }
+
+        private static void GetDogPettingAnimation()
+        {
             CompanionItem doggo = Gungeon.Game.Items["dog"].GetComponent<CompanionItem>();
             AIActor doggoai = EnemyDatabase.GetOrLoadByGuid(doggo.CompanionGuid);
             CompanionController doggocontroller =
                 doggoai.gameObject.GetOrAddComponent<CompanionController>();
-            doggoai.gameObject.AddComponent<DebugCompanionController>();
-
-            //Make Wolf pettable
-            ETGModConsole.Log("Making wolfyboi pettable");
-            CompanionItem wolfyboi = Gungeon.Game.Items["wolf"].GetComponent<CompanionItem>();
-            AIActor wolfyboiai = EnemyDatabase.GetOrLoadByGuid(wolfyboi.CompanionGuid);
-            wolfyboiai.gameObject.AddComponent<PettableCompanionController>();
-        }
-    }
-
-    public class DebugCompanionController : CompanionController
-    {
-        private void Start()
-        {
-            ETGModConsole.Log("debug companion controller loaded");
-            // CompanionBuilder.AnimationType
-            AIAnimator a = this.sprite.aiAnimator;
-            foreach(AIAnimator.NamedDirectionalAnimation n in a.OtherAnimations)
-            {
-                if (n.name == "pet")
-                {
-                    DirectionalAnimation d = n.anim;
-                    d.WriteJSON("/tmp/jsonpath");
-                    foreach(string s in d.AnimNames)
-                    {
-                        ETGModConsole.Log("aname: "+s);
-                    }
-                }
-                // "pet" is the animation we're looking for
-            }
-        }
-        public override void Update()
-        {
-            // ETGModConsole.Log(this.sprite.name);
-            // ETGModConsole.Log("updated");
-            // ETGModConsole.Log(this.spriteAnimator.m_queuedAnimationName);
-            base.Update();
-        }
-    }
-
-    public class PettableCompanionController : CompanionController
-    {
-        private void Start()
-        {
-            this.CanBePet = true;
-            CompanionItem doggo = Gungeon.Game.Items["dog"].GetComponent<CompanionItem>();
-            AIActor doggoai = EnemyDatabase.GetOrLoadByGuid(doggo.CompanionGuid);
-            CompanionController doggocontroller =
-                doggoai.gameObject.GetComponent<CompanionController>();
-
             foreach(AIAnimator.NamedDirectionalAnimation n in doggocontroller.sprite.aiAnimator.OtherAnimations)
             {
-                if (n.name == "pet")
-                {
-                    this.sprite.aiAnimator.OtherAnimations.Add(new AIAnimator.NamedDirectionalAnimation
-                    {
-                        name = "pet",
-                        anim = n.anim,
-                    });
-                }
+                if (n.name != "pet")
+                    continue;
+                dogPettingAnimation = n.anim;
+                break;
             }
+        }
 
-            // doggocontroller.sprite.aiAnimator.OtherAnimations;
+        private static string[] spritePaths = new string[]
+        {
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_001", //0
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_002", //1
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_003", //2
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_004", //3
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_005", //4
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_idle_006", //5
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_right_001", //6
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_right_002", //7
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_right_003", //8
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_left_001", //9
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_left_002", //10
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_run_left_003", //11
+            "CwaffingTheGungy/Resources/Companions/DroneCompanion/drone_pet_001", //12
+
+        };
+        public static void MakePettable(this CompanionItem ci)
+        {
+            AIActor cai            = EnemyDatabase.GetOrLoadByGuid(ci.CompanionGuid);
+            CompanionController cc = cai.gameObject.GetOrAddComponent<CompanionController>();
+            cc.CanBePet            = true;
+            AIAnimator can         = cc.sprite.aiAnimator;
+
+            // can.OtherAnimations.Add(new AIAnimator.NamedDirectionalAnimation {
+            //     name = "pet",
+            //     anim = dogPettingAnimation,
+            // });
+
+            // tk2dSpriteCollectionData cta = cc.GetComponent<tk2dSpriteCollectionData>();
+            // if(cta == null)
+            //     cta = SpriteBuilder.ConstructCollection(cc.sprite.gameObject, ("wolf" + "_Pool"));
+
+            int startindex = 0;
+            for (int i = 0; i < spritePaths.Length; i++)
+            {
+                startindex = SpriteBuilder.AddSpriteToCollection(spritePaths[i], cc.sprite.Collection);
+            }
+            //Idling Animation
+            SpriteBuilder.AddAnimation(cc.sprite.spriteAnimator, cc.sprite.Collection, new List<int>
+            {
+                startindex,
+            }, "pet", tk2dSpriteAnimationClip.WrapMode.Loop).fps = 8f;
+
+            // cai.gameObject.AddAnimation("pet", "CwaffingTheGungy/Resources/Companions/DroneCompanion/", 8,
+            //     CompanionBuilder.AnimationType.Other);
+
+            // CompanionBuilder.BuildAnimation(can, pet, spriteDirectory, fps);
             // wolfyboiai.animationAudioEvents = doggoai.animationAudioEvents;
         }
-        // public override void Update()
-        // {
-        //     // if(!(this.IsBeingPet && this.m_owner.IsPetting))
-        //         base.Update();
-        //     // else
-        //     //     this.DoPet(this.m_owner);
-        // }
     }
 }
 
