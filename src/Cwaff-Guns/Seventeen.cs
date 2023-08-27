367810e470f638e01ebd7dcdf1fb826314729be8
Patrick Crain <patrickacrain@gmail.com>
2023-08-27 14:27:27 -0400

    Figure out one-pixel collider trick for better bounce animation

--- src/Cwaff-Guns/Seventeen.cs
+++ src/Cwaff-Guns/Seventeen.cs
@@ -47,21 +47,24 @@ namespace CwaffingTheGungy
 
             var comp = gun.gameObject.AddComponent<Seventeen>();
 
+            IntVector2 colliderSize = new IntVector2(1,1); // 1-pixel collider for accurate bounce animation
+
             _ProjSpriteInactive = AnimateBullet.CreateProjectileAnimation(
                 new List<string> {
                     "bouncelet_gray_001",
                 }, 10, true, new IntVector2(14, 14),
-                false, tk2dBaseSprite.Anchor.MiddleCenter, true, true);
+                false, tk2dBaseSprite.Anchor.MiddleCenter, true, true, null, colliderSize);
             _ProjSpriteActive = AnimateBullet.CreateProjectileAnimation(
                 new List<string> {
                     "bouncelet_001",
                 }, 10, true, new IntVector2(14, 14),
-                false, tk2dBaseSprite.Anchor.MiddleCenter, true, true);
+                false, tk2dBaseSprite.Anchor.MiddleCenter, true, true, null, colliderSize);
 
             Projectile projectile = Lazy.PrefabProjectileFromGun(gun);
                 projectile.AddAnimation(_ProjSpriteActive);
                 projectile.AddAnimation(_ProjSpriteInactive);
                 projectile.baseData.damage = _Damage_After_Bounce;
+                projectile.baseData.speed  = 44;
                 projectile.gameObject.AddComponent<HarmlessUntilBounce>();
         }
     }
@@ -71,8 +74,6 @@ namespace CwaffingTheGungy
         private Projectile _projectile;
         private PlayerController _owner;
         private bool _bounceFinished = false;
-        private bool _bounceStarted = false;
-        private Vector3? _lastBouncePos = null;
 
         private void Start()
         {
@@ -82,62 +83,24 @@ namespace CwaffingTheGungy
             this._owner = pc;
 
             BounceProjModifier bounce = this._projectile.gameObject.GetOrAddComponent<BounceProjModifier>();
-                bounce.numberOfBounces     = 10; // needs to be more than 1 or projectile dies immediately in special handling code below
+                bounce.numberOfBounces     = 1; // needs to be more than 1 or projectile dies immediately in special handling code below
                 bounce.chanceToDieOnBounce = 0f;
                 bounce.OnBounce += OnBounce;
+                bounce.onlyBounceOffTiles = true;
 
             this._projectile.specRigidbody.OnPreRigidbodyCollision += this.OnPreCollision;
-            this._projectile.specRigidbody.OnPreTileCollision += this.OnPreTileCollision;
-            this._projectile.specRigidbody.OnTileCollision += this.OnTileCollision;
-            // this._projectile.BulletScriptSettings.surviveTileCollisions = true;
 
             this._projectile.SetAnimation(Seventeen._ProjSpriteInactive); // TODO: this doesn't seem to work properly; default sprite is always first sprite added
         }
 
         private void OnPreCollision(SpeculativeRigidbody myRigidbody, PixelCollider myPixelCollider, SpeculativeRigidbody otherRigidbody, PixelCollider otherPixelCollider)
         {
-            // if (this._bounceStarted && !this._bounceFinished)
-            // {
-            //     PhysicsEngine.SkipCollision = true;
-            //     return;
-            // }
-
-            if (otherRigidbody.GetComponent<AIActor>() is AIActor enemy)
-            {
-                if (!this._bounceFinished)
-                    PhysicsEngine.SkipCollision = true; // skip collision if we haven't bounced yet
-                return;
-            }
-        }
-
-        private void OnPreTileCollision(SpeculativeRigidbody myrigidbody, PixelCollider mypixelcollider, PhysicsEngine.Tile tile, PixelCollider tilepixelcollider)
-        {
-            if (this._bounceFinished)
-            {
-                PhysicsEngine.SkipCollision = false;
-                return;
-            }
-
-            if (this._bounceStarted)
-            {
-                PhysicsEngine.SkipCollision = true;
-                return;
-            }
-
-            this._bounceStarted = true;
-            _lastBouncePos = PhysicsEngine.PendingCastResult.Contact.ToVector3ZisY(0f);
-            ETGModConsole.Log($"bounce at {_lastBouncePos}");
-        }
-
-
-        private void OnTileCollision(CollisionData tileCollision)
-        {
-            ETGModConsole.Log($"  tile collision");
+            if (otherRigidbody.GetComponent<AIActor>() is AIActor && (!this._bounceFinished))
+                PhysicsEngine.SkipCollision = true; // skip collision if we haven't bounced yet
         }
 
         private void OnBounce()
         {
-            ETGModConsole.Log($"  onbounce");
             this._projectile = base.GetComponent<Projectile>();
             this._projectile.SetAnimation(Seventeen._ProjSpriteActive);
             this._projectile.StartCoroutine(DoElasticBounce());
@@ -148,39 +111,27 @@ namespace CwaffingTheGungy
             float oldSpeed = this._projectile.baseData.speed;
             Vector3 oldScale = this._projectile.spriteAnimator.transform.localScale;
 
-            this._bounceStarted = true;
-            this._projectile.specRigidbody.CollideWithOthers = false;
-            this._projectile.specRigidbody.CollideWithTileMap = false;
-            this._projectile.BulletScriptSettings.surviveTileCollisions = true;
-            this._projectile.BulletScriptSettings.surviveRigidbodyCollisions = true;
-
-            // if (_lastBouncePos.HasValue)
-            //     this._projectile.transform.position = _lastBouncePos.Value;
             this._projectile.baseData.speed = 0.001f;
             this._projectile.UpdateSpeed();
             this._projectile.specRigidbody.Reinitialize();
-            ETGModConsole.Log($"  elastic start");
+
             for (int i = 10; i > 3; --i)
             {
                 this._projectile.spriteAnimator.transform.localScale = oldScale.WithX(((float)i)/10f);
                 yield return null;
             }
-            yield return new WaitForSeconds(0.1f);
             for (int i = 3; i < 10; ++i)
             {
                 this._projectile.spriteAnimator.transform.localScale = oldScale.WithX(((float)i)/10f);
                 yield return null;
             }
             this._projectile.spriteAnimator.transform.localScale = oldScale;
+
             this._projectile.baseData.speed = oldSpeed;
             this._projectile.UpdateSpeed();
+            this._projectile.specRigidbody.Reinitialize();
 
             this._bounceFinished = true;
-            this._projectile.specRigidbody.CollideWithOthers = true;
-            this._projectile.specRigidbody.CollideWithTileMap = true;
-            this._projectile.BulletScriptSettings.surviveTileCollisions = false;
-            this._projectile.BulletScriptSettings.surviveRigidbodyCollisions = false;
-            ETGModConsole.Log($"  elastic finish");
         }
     }
 }
