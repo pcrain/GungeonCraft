a608f3d9bd821a7de944dad3a04e1215042ad4c8
Patrick Crain <patrickacrain@gmail.com>
2024-05-02 23:09:16 -0400

    Get most of Armor Piercing Rounds working

--- src/Main.cs
+++ src/Main.cs
@@ -352,6 +352,7 @@ public class Initialisation : BaseUnityPlugin
                     // Protractor.Init();  // unfinished
                     // PlaybulletMagazine.Init();
                     ScavengingArms.Init();
+                    ArmorPiercingRounds.Init();
                     setupPassivesWatch.Stop();
                 // });
                 // setupPassivesThread.Start();
