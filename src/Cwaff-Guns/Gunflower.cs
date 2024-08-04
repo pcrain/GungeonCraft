e12f9c317ec969d802cd81b7dd66edd321af8804
Patrick Crain <patrickacrain@gmail.com>
2024-08-04 14:08:21 -0400

    Finish initial version of LightAPI

--- src/Cwaff-Guns/Gunflower.cs
+++ src/Cwaff-Guns/Gunflower.cs
@@ -7,8 +7,6 @@ public class Gunflower : CwaffGun
     public static string LongDescription  = "TBD";
     public static string Lore             = "TBD";
 
-    // private float _strength;
-
     public static void Init()
     {
         Gun gun = Lazy.SetupGun<Gunflower>(ItemName, ShortDescription, LongDescription, Lore);
@@ -16,125 +14,34 @@ public class Gunflower : CwaffGun
                 muzzleFrom: Items.Mailbox, fireAudio: "paintball_shoot_sound", reloadAudio: "paintball_reload_sound");
 
         Projectile proj = gun.InitProjectile(GunData.New(sprite: null, clipSize: 12, cooldown: 0.18f, shootStyle: ShootStyle.SemiAutomatic,
-            damage: 9.0f, speed: 25f, range: 18f, force: 12f, hitEnemySound: "paintball_impact_enemy_sound", hitWallSound: "paintball_impact_wall_sound"));
-        proj.AddLight(useCone: true, fadeInTime: 0.5f, fadeOutTime: 0.25f, color: Color.red);
+            damage: 9.0f, speed: 1f, range: 1000f, force: 12f, hitEnemySound: "paintball_impact_enemy_sound", hitWallSound: "paintball_impact_wall_sound"))
+          .Attach<LightProjectile>();
+        proj.AddLight(useCone: true, fadeInTime: 0.5f, fadeOutTime: 0.25f, color: Color.red, rotateWithParent: false);
 
-        gun.AddLight(useCone: true, fadeInTime: 0.5f, fadeOutTime: 0.25f);
-
-        // gun.gameObject.AddComponent<GunflowerAmmoDisplay>();
+        gun.AddLight(useCone: true, fadeInTime: 0.5f, fadeOutTime: 0.25f, color: Color.yellow, grownIn: true, turnOnImmediately: false);
     }
 
-    // public override void OnSwitchedToThisGun()
-    // {
-    //     base.OnSwitchedToThisGun();
-    //     SetupLights();
-    // }
-
-    // public override void OnSwitchedAwayFromThisGun()
-    // {
-    //     base.OnSwitchedAwayFromThisGun();
-    //     DestroyLights();
-    // }
-
-    // public override void OnPlayerPickup(PlayerController player)
-    // {
-    //     base.OnPlayerPickup(player);
-    //     SetupLights();
-    // }
-
-    // private AdditionalBraveLight _light;
-    // private void SetupLights() // called whenever you want to attach a flashlight to your gun
-    // {
-    //     this._strength = 1f;
-    //     DestroyLights();
-    //     GameObject gameObject         = new GameObject();
-    //     gameObject.transform.position = this.gun.barrelOffset.transform.position;
-    //     gameObject.transform.parent   = this.gun.barrelOffset.transform;
-    //     this._light = gameObject.AddComponent<AdditionalBraveLight>();
-    //     this._light.LightColor              = Color.yellow;
-    //     this._light.LightIntensity          = this._strength;
-    //     this._light.LightRadius             = 15f;
-    //     this._light.UsesCone                = true;
-    //     this._light.LightAngle              = 30f; // misnomer, width of cone
-    //     this._light.LightOrient             = this.PlayerOwner.m_currentGunAngle;
-    //     this._light.Initialize();
-    // }
-
-    // public override void Update()
-    // {
-    //     base.Update();
-    //     if (!this.PlayerOwner)
-    //         return;
-    //     this._light.LightOrient    = this.PlayerOwner.m_currentGunAngle;
-    //     this._light.LightIntensity = this._strength;
-    // }
-
-    // private void DestroyLights() // called whenever you want to destroy the flashlight for your gun
-    // {
-    //     if (this._light)
-    //         UnityEngine.Object.Destroy(this._light.gameObject);
-    // }
-
-    // public override void OnDroppedByPlayer(PlayerController player)
-    // {
-    //     DestroyLights();
-    //     base.OnDroppedByPlayer(player);
-    // }
-
-    // public override void OnDestroy()
-    // {
-    //     DestroyLights();
-    //     base.OnDestroy();
-    // }
-
-    // public override void OnReloadPressed(PlayerController player, Gun gun, bool manualReload)
-    // {
-    //     base.OnReloadPressed(player, gun, manualReload);
-    //     if (!player.IsDodgeRolling && player.AcceptingNonMotionInput)
-    //         this._strength *= 10f;
-    // }
-
-    // private class GunflowerAmmoDisplay : CustomAmmoDisplay
-    // {
-    //     private Gun _gun;
-    //     private Gunflower _gunflower;
-    //     private PlayerController _owner;
-    //     private void Start()
-    //     {
-    //         this._gun = base.GetComponent<Gun>();
-    //         this._gunflower = this._gun.GetComponent<Gunflower>();
-    //         this._owner = this._gun.CurrentOwner as PlayerController;
-    //     }
-
-    //     public override bool DoCustomAmmoDisplay(GameUIAmmoController uic)
-    //     {
-    //         if (!this._owner)
-    //             return false;
-
-    //         uic.GunAmmoCountLabel.Text = $"Brightness: {this._gunflower._strength}";
-    //         return true;
-    //     }
-    // }
+    public override void OnReloadPressed(PlayerController player, Gun gun, bool manualReload)
+    {
+        base.OnReloadPressed(player, gun, manualReload);
+        if (gun.GetComponentInChildren<EasyLight>() is EasyLight light)
+            light.Toggle();
+    }
 }
-
 
 public class LightProjectile : MonoBehaviour
 {
-    private Projectile _projectile;
-    private PlayerController _owner;
+    private GameActor _owner;
+    private EasyLight _light;
     private void Start()
     {
-        this._projectile = base.GetComponent<Projectile>();
-        this._owner = this._projectile.Owner as PlayerController;
+        this._light = base.gameObject.GetComponentInChildren<EasyLight>();
+        this._owner = base.gameObject.GetComponent<Projectile>().Owner;
     }
 
     private void Update()
     {
-      // enter update code here
-    }
-
-    private void OnDestroy()
-    {
-      // enter destroy code here
+        if (this._light && this._owner)
+            this._light.PointAt(this._owner.CenterPosition);
     }
 }
--- src/Cwaff-Misc/LightAPI.cs
+++ src/Cwaff-Misc/LightAPI.cs
@@ -2,25 +2,210 @@ namespace CwaffingTheGungy;
 
 using static EasyLight.State;
 
-public class EasyLight : MonoBehaviour
+/// <summary>Helper class for adding nice looking lights to Guns, Projectiles, and other GameObject</summary>
+public partial class EasyLight : MonoBehaviour
 {
-  public AdditionalBraveLight _light;
-  public GameObject _parentObj;
-  public float _maxLifetime;
-  public float _maxBrightness;
-  public float _fadeInTime;
-  public float _fadeOutTime;
-  public float _fadeOutStartTime;
-  public bool _destroyWithParent;
-  public bool _usesLifeTime;
-  public bool _rotateWithParent;
-  public bool _turnOnImmediately;
+  /// <summary>Whether the light is current on or not. May still be invisible if its parent is deactivated.</summary>
+  public bool IsOn => this._state == VISIBLE || this._state == FADEIN;
+
+  /// <summary>Turns the light on if off, and turns it off if on.</summary>
+  public void Toggle()
+  {
+    if (this.IsOn)
+      TurnOff();
+    else
+      TurnOn();
+  }
+
+  /// <summary>Turns the light on.</summary>
+  public void TurnOn()
+  {
+    if (this.IsOn)
+      return;
+    if (this._fadeInTime > 0f)
+    {
+      this._currentFadeTime = 0f;
+      this._light.LightIntensity = 0f;
+      this._state = FADEIN;
+      return;
+    }
+    this._light.LightIntensity = this._brightness;
+    this._state = VISIBLE;
+  }
+
+  /// <summary>Turns the light off, optionally skipping its fade out animation.</summary>
+  public void TurnOff(bool immediate = false)
+  {
+    if (!this.IsOn)
+      return;
+    if (!immediate && this._fadeOutTime > 0f)
+    {
+      this._currentFadeTime = 0f;
+      this._light.LightIntensity = this._brightness;
+      this._state = FADEOUT;
+      return;
+    }
+    this._light.LightIntensity = 0f;
+    this._state = HIDDEN;
+  }
+
+  /// <summary>Sets the brightness of a light.</summary>
+  public void SetBrightness(float brightness)
+  {
+    this._brightness = brightness;
+  }
+
+  /// <summary>Sets how far the light extends from its origin point.</summary>
+  public void SetRadius(float radius)
+  {
+    this._radius = radius;
+  }
+
+  /// <summary>Sets the angle spanned by cone lights.</summary>
+  public void SetConeWidth(float coneWidth)
+  {
+    this._light.LightAngle = coneWidth;
+  }
+
+  /// <summary>Sets the color of the light.</summary>
+  public void SetColor(Color color)
+  {
+    this._light.LightColor = color;
+  }
+
+  /// <summary>For cone lights, automatically tracks the specified GameObject's transform.</summary>
+  public void TrackObject(GameObject g)
+  {
+    this._trackedObject = g;
+  }
+
+  /// <summary>For cone lights, points the light in the direction specified by angle.</summary>
+  public void PointInDirection(float angle)
+  {
+    this._light.LightOrient = angle;
+  }
+
+  /// <summary>For cone lights, points the light towards the specified position.</summary>
+  public void PointAt(Vector2 position)
+  {
+    this._light.LightOrient = (position - this._light.transform.position.XY()).ToAngle();
+  }
+
+  /// <summary>Creates an Easy Light at the specified positinon and / or parented to the specified object</summary>
+  /// <param name="pos">Where to place the light. Can be ignored if parent is non-null. Must be set if parent is null.</param>
+  /// <param name="parent">The transform to parent the light to. If null, the light cannot be moved from its initial position.</param>
+  /// <param name="color">The color of the light. Can be changed later using SetColor().</param>
+  /// <param name="maxLifeTime">If greater than zero, determines the amount of time before the light is destroyed.</param>
+  /// <param name="radius">How far the light radiates from its origin point.</param>
+  /// <param name="growIn">If true, the light will grow to its radius when fading in and shrink to nothingness when fading out.</param>
+  /// <param name="brightness">The brightness of the light. Setting to anything lower than 3.5f can have strange effects. Can be changed later using SetBrightness().</param>
+  /// <param name="fadeInTime">If greater than 0, determines how long the light takes to reach its max brightness when turned on.</param>
+  /// <param name="fadeOutTime">If greater than 0, determines how long the light takes to reach zero brightness when turned off.</param>
+  /// <param name="destroyWithParent">(MIGHT NOT WORK) If true, the light is destroyed when its parent is destroyed. If false, the light persists after its parent is destroyed.</param>
+  /// <param name="useCone">If true, the light will be emitted in a cone instead of in all directions.</param>
+  /// <param name="coneWidth">If useCone is true, determines the angle spanned by the cone of light. Can be changed later using SetConeWidth().</param>
+  /// <param name="coneDirection">If useCone is true, determines the angle the cone of light is pointed at. Can be changed later using PointInDirection() or PointAt().</param>
+  /// <param name="rotateWithParent">If true, if useCone is true, and if parented to a Gun / Projectile, automatically points the cone in the direction of the gun barrel / projectile respectively.</param>
+  /// <param name="turnOnImmediately">If true, the light is turned on immediately when created. If false, the light must be turned on manually with Toggle() or TurnOn().</param>
+  public static EasyLight Create(Vector2? pos = null, Transform parent = null, Color? color = null, float maxLifeTime = -1f,  float radius = 20f, bool grownIn = false, float brightness = 10f,
+      float fadeInTime = 0f, float fadeOutTime = 0f, bool destroyWithParent = true, bool useCone = false, float coneWidth = 30f, float coneDirection = 0f,
+      bool rotateWithParent = true, bool turnOnImmediately = true)
+  {
+    return EasyLight.CreateInternal(
+      pos               : pos,
+      parent            : parent,
+      color             : color,
+      maxLifeTime       : maxLifeTime,
+      radius            : radius,
+      growIn            : grownIn,
+      brightness        : brightness,
+      fadeInTime        : fadeInTime,
+      fadeOutTime       : fadeOutTime,
+      destroyWithParent : destroyWithParent,
+      useCone           : useCone,
+      coneWidth         : coneWidth,
+      coneDirection     : coneDirection,
+      rotateWithParent  : rotateWithParent,
+      turnOnImmediately : turnOnImmediately
+      );
+  }
+}
+
+public static class EasyLightExtensions
+{
+  /// <summary>Attaches an EasyLight to a gun.</summary>
+  /// <param name="brightness">The brightness of the light. Setting to anything lower than 3.5f can have strange effects. Can be changed later using SetBrightness().</param>
+  /// <param name="color">The color of the light. Can be changed later using SetColor().</param>
+  /// <param name="useCone">If true, the light will be emitted in a cone instead of in all directions.</param>
+  /// <param name="turnOnImmediately">If true, the light is turned on immediately when created. If false, the light must be turned on manually with Toggle() or TurnOn().</param>
+  /// <param name="fadeInTime">If greater than 0, determines how long the light takes to reach its max brightness when turned on.</param>
+  /// <param name="fadeOutTime">If greater than 0, determines how long the light takes to reach zero brightness when turned off.</param>
+  /// <param name="coneWidth">If useCone is true, determines the angle spanned by the cone of light. Can be changed later using SetConeWidth().</param>
+  /// <param name="rotateWithParent">If true, if useCone is true, and if parented to a Gun / Projectile, automatically points the cone in the direction of the gun barrel / projectile respectively.</param>
+  /// <param name="radius">How far the light radiates from its origin point.</param>
+  /// <param name="growIn">If true, the light will grow to its radius when fading in and shrink to nothingness when fading out.</param>
+  public static EasyLight AddLight(this Gun gun, float brightness = 10f, Color? color = null, bool useCone = false, bool turnOnImmediately = true, float fadeInTime = 0f,
+    float fadeOutTime = 0f, float coneWidth = 30f, bool rotateWithParent = true, float radius = 20f, bool grownIn = false)
+  {
+    return EasyLight.CreateInternal(parent: gun.barrelOffset, brightness: brightness, fadeInTime: fadeInTime, fadeOutTime: fadeOutTime, color: color, radius: radius,
+      maxLifeTime: -1f, useCone: useCone, coneWidth: coneWidth, turnOnImmediately: turnOnImmediately, rotateWithParent: rotateWithParent, growIn: grownIn);
+  }
+
+  /// <summary>Attaches an EasyLight to a projectile.</summary>
+  /// <param name="brightness">The brightness of the light. Setting to anything lower than 3.5f can have strange effects. Can be changed later using SetBrightness().</param>
+  /// <param name="color">The color of the light. Can be changed later using SetColor().</param>
+  /// <param name="useCone">If true, the light will be emitted in a cone instead of in all directions.</param>
+  /// <param name="turnOnImmediately">If true, the light is turned on immediately when created. If false, the light must be turned on manually with Toggle() or TurnOn().</param>
+  /// <param name="fadeInTime">If greater than 0, determines how long the light takes to reach its max brightness when turned on.</param>
+  /// <param name="fadeOutTime">If greater than 0, determines how long the light takes to reach zero brightness when turned off.</param>
+  /// <param name="coneWidth">If useCone is true, determines the angle spanned by the cone of light. Can be changed later using SetConeWidth().</param>
+  /// <param name="rotateWithParent">If true, if useCone is true, and if parented to a Gun / Projectile, automatically points the cone in the direction of the gun barrel / projectile respectively.</param>
+  /// <param name="radius">How far the light radiates from its origin point.</param>
+  /// <param name="growIn">If true, the light will grow to its radius when fading in and shrink to nothingness when fading out.</param>
+  public static EasyLight AddLight(this Projectile projectile, float brightness = 10f, Color? color = null, bool useCone = false, bool turnOnImmediately = true, float fadeInTime = 0f,
+    float fadeOutTime = 0f, float coneWidth = 30f, bool rotateWithParent = true, float radius = 20f, bool grownIn = false)
+  {
+    return EasyLight.CreateInternal(parent: projectile.gameObject.transform, brightness: brightness, fadeInTime: fadeInTime, fadeOutTime: fadeOutTime, color: color, radius: radius,
+      maxLifeTime: -1f, useCone: useCone, coneWidth: coneWidth, turnOnImmediately: turnOnImmediately, rotateWithParent: rotateWithParent, growIn: grownIn);
+  }
+}
+
+// Private API for EasyLight
+public partial class EasyLight : MonoBehaviour
+{
+  [SerializeField]
+  private AdditionalBraveLight _light;
+  [SerializeField]
+  private GameObject _parentObj;
+  [SerializeField]
+  private float _maxLifetime;
+  [SerializeField]
+  private float _brightness;
+  [SerializeField]
+  private float _fadeInTime;
+  [SerializeField]
+  private float _fadeOutTime;
+  [SerializeField]
+  private float _fadeOutStartTime;
+  [SerializeField]
+  private bool _destroyWithParent;
+  [SerializeField]
+  private bool _usesLifeTime;
+  [SerializeField]
+  private bool _rotateWithParent;
+  [SerializeField]
+  private bool _turnOnImmediately;
+  [SerializeField]
+  private bool _growIn;
+  [SerializeField]
+  private float _radius;
 
   private float _lifetime;
   private float _currentFadeTime;
   private State _state;
   private Gun _gun;
   private Projectile _proj;
+  private GameObject _trackedObject;
 
   internal enum State
   {
@@ -30,10 +215,15 @@ public class EasyLight : MonoBehaviour
     FADEOUT,
   }
 
-  internal static EasyLight Create(Transform parent = null, Vector2? pos = null, Color? color = null, float maxLifeTime = 1f, float brightness = 10f, float radius = 20f,
+  internal static EasyLight CreateInternal(Vector2? pos = null, Transform parent = null, Color? color = null, float maxLifeTime = 1f, float brightness = 10f, float radius = 20f,
     float fadeInTime = 0f, float fadeOutTime = 0f, bool destroyWithParent = true, bool useCone = false, float coneWidth = 30f, float coneDirection = 0f,
-    bool rotateWithParent = true, bool turnOnImmediately = true)
+    bool rotateWithParent = true, bool turnOnImmediately = true, bool growIn = false)
   {
+    if (pos == null && parent == null)
+    {
+      ETGModConsole.Log($"can't create light without parent or position");
+      return null;
+    }
     GameObject lightObj = new GameObject("easylight");
     EasyLight e = lightObj.AddComponent<EasyLight>();
 
@@ -49,7 +239,7 @@ public class EasyLight : MonoBehaviour
 
     e._light = lightObj.AddComponent<AdditionalBraveLight>();
     e._light.LightColor = color ?? Color.white;
-    e._light.LightRadius = radius;
+    e._radius = e._light.LightRadius = radius;
     if (useCone)
     {
       e._light.UsesCone = true;
@@ -65,8 +255,9 @@ public class EasyLight : MonoBehaviour
     e._fadeInTime = fadeInTime;
     e._fadeOutTime = Mathf.Max(fadeOutTime, 0f);
     e._fadeOutStartTime = maxLifeTime - e._fadeOutTime;
-    e._maxBrightness = brightness;
+    e._brightness = brightness;
     e._turnOnImmediately = turnOnImmediately;
+    e._growIn = growIn;
 
     return e;
   }
@@ -86,7 +277,7 @@ public class EasyLight : MonoBehaviour
     }
     else
     {
-      this._light.LightIntensity = this._maxBrightness;
+      this._light.LightIntensity = this._brightness;
       this._state = State.VISIBLE;
     }
     if (this._parentObj)
@@ -98,36 +289,6 @@ public class EasyLight : MonoBehaviour
       else if (this._parentObj.transform.parent is Transform grandparent && grandparent.gameObject.GetComponentInChildren<Gun>() is Gun gun2)
         this._gun = gun2;
     }
-  }
-
-  public void TurnOn()
-  {
-    if (this._state == VISIBLE || this._state == FADEIN)
-      return;
-    if (this._fadeInTime > 0f)
-    {
-      this._currentFadeTime = 0f;
-      this._light.LightIntensity = 0f;
-      this._state = FADEIN;
-      return;
-    }
-    this._light.LightIntensity = this._maxBrightness;
-    this._state = VISIBLE;
-  }
-
-  public void TurnOff(bool immediate = false)
-  {
-    if (this._state == HIDDEN || this._state == FADEOUT)
-      return;
-    if (!immediate && this._fadeOutTime > 0f)
-    {
-      this._currentFadeTime = 0f;
-      this._light.LightIntensity = this._maxBrightness;
-      this._state = FADEOUT;
-      return;
-    }
-    this._light.LightIntensity = 0f;
-    this._state = HIDDEN;
   }
 
   private void OnDisable()
@@ -158,14 +319,19 @@ public class EasyLight : MonoBehaviour
         this._currentFadeTime += BraveTime.DeltaTime;
         if (this._currentFadeTime >= this._fadeInTime)
         {
-          this._light.LightIntensity = this._maxBrightness;
+          this._light.LightIntensity = this._brightness;
+          this._light.LightRadius = this._radius;
           this._state = VISIBLE;
           break;
         }
         float percentFadeInLeft = 1f - this._currentFadeTime / this._fadeInTime;
-        this._light.LightIntensity = (1f - percentFadeInLeft * percentFadeInLeft) * this._maxBrightness; // ease in
+        float easeInFraction = 1f - percentFadeInLeft * percentFadeInLeft;
+        this._light.LightIntensity = easeInFraction * this._brightness;
+        if (this._growIn)
+          this._light.LightRadius = easeInFraction * this._radius;
         break;
       case VISIBLE:
+        this._light.LightRadius = this._radius;
         if (!this._usesLifeTime)
           break;
         if (this._lifetime < this._fadeOutStartTime)
@@ -186,20 +352,30 @@ public class EasyLight : MonoBehaviour
           return;
         }
         float percentFadeOutLeft = 1f - this._currentFadeTime / this._fadeOutTime;
-        this._light.LightIntensity = (percentFadeOutLeft * percentFadeOutLeft) * this._maxBrightness; // ease out
+        float easeOutFraction = percentFadeOutLeft * percentFadeOutLeft;
+        this._light.LightIntensity = easeOutFraction * this._brightness; // ease out
+        if (this._growIn)
+          this._light.LightRadius = easeOutFraction * this._radius;
         break;
       default:
         break;
     }
 
-    if (this._parentObj && this._light.UsesCone && this._rotateWithParent)
+    if (this._light.UsesCone)
     {
-      if (this._proj)
-        this._light.LightOrient = this._proj.transform.right.XY().ToAngle();
-      else if (this._gun)
-        this._light.LightOrient = this._gun.CurrentAngle;
-      else
-        this._light.LightOrient = this._parentObj.transform.localRotation.z;
+      if (this._trackedObject)
+      {
+        this._light.LightOrient = (this._trackedObject.transform.position - this._light.transform.position).XY().ToAngle();
+      }
+      else if (this._parentObj && this._rotateWithParent)
+      {
+        if (this._proj)
+          this._light.LightOrient = this._proj.transform.right.XY().ToAngle();
+        else if (this._gun)
+          this._light.LightOrient = this._gun.CurrentAngle;
+        else
+          this._light.LightOrient = this._parentObj.transform.localRotation.z;
+      }
     }
   }
 
@@ -237,28 +413,3 @@ public class EasyLight : MonoBehaviour
   }
 }
 
-public static class LightAPI
-{
-  public static EasyLight AddLight(this Gun gun, float brightness = 10f, Color? color = null, bool useCone = false, bool turnOnImmediately = true, float fadeInTime = 0f, float fadeOutTime = 0f, float coneWidth = 30f)
-  {
-    return EasyLight.Create(parent: gun.barrelOffset, brightness: brightness, fadeInTime: fadeInTime, fadeOutTime: fadeOutTime, color: color,
-      maxLifeTime: -1f, useCone: useCone, coneWidth: coneWidth, turnOnImmediately: turnOnImmediately);
-  }
-
-  public static EasyLight AddLight(this Projectile projectile, float brightness = 10f, Color? color = null, bool useCone = false, bool turnOnImmediately = true, float fadeInTime = 0f,
-    float fadeOutTime = 0f, float coneWidth = 30f)
-  {
-    return EasyLight.Create(parent: projectile.gameObject.transform, brightness: brightness, fadeInTime: fadeInTime, fadeOutTime: fadeOutTime, color: color,
-      maxLifeTime: -1f, useCone: useCone, coneWidth: coneWidth, turnOnImmediately: turnOnImmediately);
-  }
-
-  public static EasyLight TemporaryLight(Vector2 position, float maxLifeTime = 1f, float brightness = 10f, float fadeInTime = 0f, float fadeOutTime = 0f, Transform parent = null)
-  {
-    return null;
-  }
-
-  public static EasyLight FancyLight(Vector2 position, float maxLifeTime = 1f, float brightness = 10f, float fadeInTime = 0f, float fadeOutTime = 0f, Transform parent = null)
-  {
-    return null;
-  }
-}
