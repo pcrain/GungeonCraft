1a859492964040b8cead4d4df25c035f8a9e0c72
Patrick Crain <patrickacrain@gmail.com>
2025-05-07 23:00:56 -0400

    Port over most vanilla bugfixes and optimizations to Gungeon Go Vroom

--- src/Cwaff-Misc/Hotfixes.cs
+++ src/Cwaff-Misc/Hotfixes.cs
@@ -27,6 +27,7 @@ public static class BasicBeamControllerFrameUpdatePatch
 }
 
 // Fixes UI armor sprites slowly shifting offscreen whenever they're changed
+//   (doesn't crop up in Vanilla due to armor sprite never changing)
 [HarmonyPatch(typeof(GameUIHeartController), nameof(GameUIHeartController.UpdateHealth))]
 public static class ArmorUIOffsetFix
 {
@@ -53,29 +54,9 @@ public static class ArmorUIOffsetFix
     private static float Zero(float originalValue) => 0f;
 }
 
-// Keeps projectile trails from disappearing if projectiles slow down too much
-[HarmonyPatch(typeof(TrailController), nameof(TrailController.Update))]
-public static class TrailControllerUpdatePatch
-{
-    [HarmonyILManipulator]
-    private static void TrailControllerUpdateIL(ILContext il)
-    {
-        ILCursor cursor = new ILCursor(il);
-        if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchCallvirt(typeof(LinkedList<TrailController.Bone>).GetMethod("get_Count"))))
-            return;
-
-        cursor.Emit(OpCodes.Ldarg_0); // TrailController
-        cursor.CallPrivate(typeof(TrailControllerUpdatePatch), nameof(KeepAliveWhenEmpty));
-    }
-
-    private static int KeepAliveWhenEmpty(int oldCount, TrailController trail)
-    {
-        return trail.destroyOnEmpty ? oldCount : 999; // must be nonzero to avoid BrTrue statement
-    }
-}
-
 // Fixes vanilla bug where if ModulesAreTiers is true, inactive burst modules contribute towards m_midBurstFire checks,
 //   causing infinite firing after letting go of mouse
+//   (doesn't crop up in vanilla due to vanilla ModulesAreTiers guns not having infinite burst sizes)
 [HarmonyPatch(typeof(Gun), nameof(Gun.ContinueAttack))]
 public static class ModulesAreTiersBurstFirePatch
 {
@@ -99,39 +80,6 @@ public static class ModulesAreTiersBurstFirePatch
       if (gun.m_currentStrengthTier == ((mod.CloneSourceIndex < 0) ? i : mod.CloneSourceIndex))
         return unadjustBurstShotCount;
       return 0;
-    }
-}
-
-// Fixes a vanilla bug with the background of final projectiles rendering above the foreground
-// Also fixes a vanilla bug with AmmoBurstVFX rendering below final clip projectiles
-[HarmonyPatch(typeof(GameUIAmmoController), nameof(GameUIAmmoController.UpdateAmmoUIForModule))]
-public static class GameUIAmmoControllerUpdateAmmoUIForModulePatch
-{
-    static void Postfix(GameUIAmmoController __instance, ref dfTiledSprite currentAmmoFGSprite, ref dfTiledSprite currentAmmoBGSprite, List<dfTiledSprite> AddlModuleFGSprites, List<dfTiledSprite> AddlModuleBGSprites, dfSprite ModuleTopCap, dfSprite ModuleBottomCap, ProjectileModule module, Gun currentGun, ref GameUIAmmoType.AmmoType cachedAmmoTypeForModule, ref string cachedCustomAmmoTypeForModule, ref int cachedShotsInClip, bool didChangeGun, int numberRemaining)
-    {
-        if (AddlModuleBGSprites == null || AddlModuleBGSprites.Count < 1)
-            return;
-        if (AddlModuleFGSprites == null || AddlModuleFGSprites.Count < 1)
-            return;
-        AddlModuleFGSprites[0].ZOrder = AddlModuleBGSprites[0].ZOrder + 1;
-        if (currentAmmoFGSprite != null)
-            currentAmmoFGSprite.ZOrder = AddlModuleFGSprites[0].ZOrder + 1;
-    }
-}
-
-// Fixes a vanilla bug where hovering guns shoot from the wrong place if created while the player is facing left
-public static class HoveringGunPatch
-{
-    [HarmonyPatch(typeof(HoveringGunController), nameof(HoveringGunController.Initialize))]
-    private class HoveringGunDoesntShootProperlyWhenCreatedWhileFacingLeftPatch
-    {
-        static void Postfix(HoveringGunController __instance)
-        {
-            if (!__instance.m_owner || !__instance.m_owner.sprite || !__instance.m_owner.sprite.FlipX)
-                return;
-            Transform t = __instance.m_shootPointTransform;
-            t.localPosition = t.localPosition.WithY(-t.localPosition.y);
-        }
     }
 }
 
@@ -175,86 +123,6 @@ public static class AmmonomiconPageRendererHotfix
         private static bool ShouldSuppressItemFromVictoryScreen(AmmonomiconPageRenderer renderer, List<PassiveItem> items, int index)
         {
             return items[index].encounterTrackable && items[index].encounterTrackable.SuppressInInventory;
-        }
-    }
-}
-
-// All 3 of Gungeon's list-shuffling implementations are flawed due to off by one errors, so we need to fix them
-//   We can't hook generic methods directly, so focus on GenerationShuffle<int>(), which is the main issue for floor room generation
-public static class RoomShuffleOffByOneHotfix
-{
-    [HarmonyPatch]
-    private class RoomShuffleOffByOnePatch
-    {
-        static MethodBase TargetMethod() {
-          // refer to C# reflection documentation:
-          return typeof(BraveUtility).GetMethod("GenerationShuffle", BindingFlags.Static | BindingFlags.Public).MakeGenericMethod(typeof(int));
-        }
-
-        [HarmonyILManipulator]
-        private static void GenerationShuffleFixIL(ILContext il)
-        {
-            ILCursor cursor = new ILCursor(il);
-            // cursor.DumpILOnce("GenerationShuffleFixIL");
-
-            // GenerationRandomRange(0, num) should be GenerationRandomRange(0, num + 1)
-            if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdloc(0)))
-                return;
-            cursor.Emit(OpCodes.Ldc_I4_1);
-            cursor.Emit(OpCodes.Add);
-
-            // num > 1 should be num >= 1
-            ILLabel forLabel = null;
-            if (!cursor.TryGotoNext(MoveType.Before, instr => instr.MatchBgt(out forLabel)))
-                return;
-            cursor.Remove();
-            cursor.Emit(OpCodes.Bge, forLabel);
-        }
-    }
-}
-
-// Quick restart doesn't call PreprcoessRun(), so once-per-run rooms will never respawn until you return to the Breach
-// GameManager.Instance.GlobalInjectionData.PreprocessRun();
-public static class QuickRestartRoomCacheHotfix
-{
-    [HarmonyPatch(typeof(GameManager), nameof(GameManager.QuickRestart))]
-    private class QuickRestartRoomCachePatch
-    {
-        [HarmonyILManipulator]
-        private static void OnQuickRestartIL(ILContext il)
-        {
-            ILCursor cursor = new ILCursor(il);
-
-            if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchLdstr("Quick Restarting...")))
-                return;
-            cursor.Index++; // skip over Debug.Log() call
-
-            // if (!cursor.TryGotoNext(MoveType.After, instr => instr.MatchCall<GameManager>("FlushAudio")))
-            //     return;
-
-            cursor.Emit(OpCodes.Ldarg_0); // load the game manager
-            cursor.CallPrivate(typeof(QuickRestartRoomCacheHotfix), nameof(ForcePreprocessRunForQuickStart));
-        }
-    }
-
-    private static void ForcePreprocessRunForQuickStart(GameManager gm)
-    {
-        // ETGModConsole.Log($"  forcibly preprocessing run");
-        if (!gm)
-            return;
-        gm.GlobalInjectionData.PreprocessRun();
-    }
-}
-
-// Duct tape gun ids aren't serialized, so dropping them clears out the duct tape gun list and breaks save serialization
-public static class DuctTapeSaveLoadHotfix
-{
-    [HarmonyPatch(typeof(Gun), nameof(Gun.CopyStateFrom))]
-    private class DuctTapeSaveLoadPatch
-    {
-        static void Postfix(Gun __instance, Gun other)
-        {
-            __instance.DuctTapeMergedGunIDs = other.DuctTapeMergedGunIDs;
         }
     }
 }
@@ -303,43 +171,6 @@ public static class SetupSynergySpritePatch
         int trimmedId = ammonomiconCollection.GetSpriteIdByName(trimmedSpriteName, defaultValue: -1);
         if (trimmedId != -1)
             __instance.notificationSynergySprite.SetSprite(ammonomiconCollection, trimmedId);
-    }
-}
-
-// Fix player two not getting Turbo Mode speed buffs in Coop
-public static class CoopTurboModeHotfix
-{
-    [HarmonyPatch(typeof(PlayerController), nameof(PlayerController.UpdateTurboModeStats))]
-    private class CoopTurboModePatch
-    {
-        [HarmonyILManipulator]
-        private static void CoopTurboModeFixHookIL(ILContext il)
-        {
-            ILCursor cursor = new ILCursor(il);
-
-            if (!cursor.TryGotoNext(MoveType.After,
-              instr => instr.MatchLdfld<PlayerController>("m_turboSpeedModifier"),
-              instr => instr.OpCode == OpCodes.Callvirt))  // can't match List<StatModified>::Add() for some reason
-                return; // failed to find what we need
-
-            // Recalculate stats after adjusting turbo speed modifier
-            cursor.Emit(OpCodes.Ldarg_0);
-            cursor.CallPrivate(typeof(CoopTurboModePatch), nameof(RecalculateTurboStats));
-
-            if (!cursor.TryGotoNext(MoveType.After,
-              instr => instr.MatchLdfld<PlayerController>("m_turboRollSpeedModifier"),
-              instr => instr.OpCode == OpCodes.Callvirt))  // can't match List<StatModified>::Add() for some reason
-                return; // failed to find what we need
-
-            // Recalculate stats after adjusting turbo roll speed modifier
-            cursor.Emit(OpCodes.Ldarg_0);
-            cursor.CallPrivate(typeof(CoopTurboModePatch), nameof(RecalculateTurboStats));
-        }
-
-        private static void RecalculateTurboStats(PlayerController player)
-        {
-            player.stats.RecalculateStats(player);
-        }
     }
 }
 
